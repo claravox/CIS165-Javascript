94e1ae616412b2662b2d4935f5838bf78f354ad5
claravox <claravox@users.noreply.github.com>
2017-05-23 20:20:34 -0700

    Delete now works

--- spread-sheet/js/spreadsheet.js
+++ spread-sheet/js/spreadsheet.js
@@ -1,27 +1,39 @@
 $(document).ready(function() {
-    /*
-    $("#plus-sign").parent().hover(function() {
-        $("#plus-sign").text("+");
-    }).mouseout(function() {
-        $("#plus-sign").text("");
-    });
-    */
     $("#datepicker").datepicker({
     	showButtonPanel: true
     });
 
-    //$("#calculate").click(addHours);
+    //$("#hours").spinner;
+
+    $("#calculate").click(addHours);
 
     $("#org").focus();
-    var tableRowArray = [];
 
-    var tableRowYa = $("#submit").click(addToTable);
-    console.log(tableRow);
-    tableRowArray.push(tableRow);
-    console.log(tableRowArray);
+    $("#submit").click(addToTable);
+
 });
 
 var i = 0;
+var tableRowArray = [];
+
+function showControls(event) {
+    $(".editbox").html("<input type=\"submit\" class=\"delete\" name=\"delete\" value=\"Delete\">");
+    $(".delete").click(deleteRow);
+}
+
+function hideControls(event) {
+    $(".editbox").text(" ");
+}
+
+function deleteRow() {
+    var rowToDelete = $(this).parent().parent().parent().hide().attr("id");
+    var numOfRow = rowToDelete.substring(3);
+    for (var j = 0; j < tableRowArray.length; j++) {
+        if (j == numOfRow) {
+            tableRowArray[j].ignore = true;
+        }
+    }
+}
 
 function addToTable(event) {
     var tableRow = {};
@@ -38,16 +50,38 @@ function addToTable(event) {
 */
     //I'd like to be able to do this with a for loop,
     //but as you see from the code block above, it didn't work.
-    $("#volunteerTable tr:last").after("<tr id=\"row" + i + "\"  class=\"row\"><td>" + tableRow.org + "</td><td class=\"hours\">" + tableRow.hours + "</td><td>" + tableRow.turnedIn + "</td><td>" + tableRow.datePicker + "</td></tr>");
+    $("#volunteerTable tr:last").after("<tr id=\"row" + i + "\"  class=\"row\"><td>" + tableRow.org + "</td><td class=\"hours\">" + tableRow.hours + "</td><td>" + tableRow.turnedIn + "</td><td>" + tableRow.datePicker + "</td><td><span class=\"editbox\"> </span></td></tr>");
+
+    //Add to i for the next time the Add button is clicked
     i++;
+
+    console.log(tableRowArray);
+
+        //Gives error:
+    //$("#add-row").insertBefore($("#add-row").before);
+
+    tableRowArray.push(tableRow);
+
+    //Show and hide delete button
+    $(".editbox").parent().hover(showControls, hideControls);
+
+
     return tableRow;
 }
 
-/*function addHours() {
-    $(".row").each(function() {
-        var totalHours;
-        totalHours += $(this ".hours").text();
+function addHours() {
+    var totalHours = 0;
 
-    });
-        console.log("Hey" + totalHours);
-}*/
+    //I used j because I had already used i for the ids of the new rows.
+    for (var j = 0; j < tableRowArray.length; j++) {
+        if (!tableRowArray[j].ignore) {
+            totalHours += Number(tableRowArray[j].hours);
+        }
+    }
+
+    if (totalHours == 1) {
+        $("#total-hours").text(totalHours + " hour");
+    } else {
+        $("#total-hours").text(totalHours + " hours");
+    }
+}
