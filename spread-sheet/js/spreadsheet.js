b8577e32f4ba000259f2f32a85dad54e20d82e05
claravox <claravox@users.noreply.github.com>
2017-05-24 19:32:19 -0700

    Spreadsheet now functioning well

--- spread-sheet/js/spreadsheet.js
+++ spread-sheet/js/spreadsheet.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
+    //Turns the date text field into a date picker.
     $("#datepicker").datepicker({
-    	showButtonPanel: true
+    	showButtonPanel: true,
+        maxDate: "+0d"
     });
 
-    //$("#hours").spinner;
+    $("#hours").spinner();
+
+    $( "input[type=submit], button, .delete, .edit" ).button();
 
     $("#calculate").click(sumUpHours);
 
     $("#org").focus();
 
     $("#submit").click(addToTable);
+
+
 
 });
 
@@ -18,15 +24,29 @@ var tableRowArray = [];
 var currentlyEditingRow = false;
 var numOfRow = 0;
 
+//var newRowContents = "<tr id=\"row" + numOfRow + "\"  class=\"row\"><td>" + tableRow.org + "</td><td class=\"hours\">" + tableRow.hours + "</td><td>" + tableRow.turnedIn + "</td><td>" + tableRow.datePicker + "</td><td><span class=\"editbox\"> </span></td></tr>"
+
+
+function checkValidity() {
+    if ($(this).spinner( "isValid" )) {
+        console.log("valid");
+    } else {
+        console.log("Bad bad");
+    }
+}
+
 function showControls(event) {
-    $(".editbox").html("<input type=\"submit\" class=\"delete\" name=\"delete\" value=\"Delete\"><input type=\"submit\" class=\"edit\" name=\"edit\" value=\"Edit\">");
+    $(".editbox").html("<button type=\"submit\" class=\"delete\" name=\"delete\" value=\"Delete\"><i class=\"fa fa-trash\"></i></button><button type=\"submit\" class=\"edit\" name=\"edit\" value=\"Edit\"><i class=\"fa fa-edit\"></i></button>");
+    $( ".delete, .edit" ).button()
     $(".delete").click(deleteRow);
     $(".edit").click(editRow);
 }
 
-function hideControls(event) {
-    $(".editbox").text(" ");
-}
+//At one time I planned to hide the controls when you moved the mouse away,
+//but it was too distracting.
+/*function hideControls(event) {
+    $(".editbox").css({color: "red"});
+}*/
 
 function deleteRow() {
     var rowToDelete = $(this).parent().parent().parent().hide().attr("id");
@@ -43,28 +63,50 @@ function editRow() {
     numOfRow = rowToEdit.substring(3);
     for (var j = 0; j < tableRowArray.length; j++) {
         if (j == numOfRow) {
-            //Do something!
+
+
             $("#add-row").effect("highlight", 1000);
             $("#add-row #org").val(tableRowArray[j].org);
             $("#add-row #hours").val(tableRowArray[j].hours);
-            $("#add-row #turned-in").val(tableRowArray[j].turnedIn);
+            //$("#add-row #turned-in").val(tableRowArray[j].turnedIn);
+            $("#add-row #turned-in").val(processCheckMarkOut(tableRowArray[j].turnedIn, "#turned-in"));
             $("#add-row #datepicker").val(tableRowArray[j].datePicker);
+
+            $("#submit").html("<i class=\"fa fa-edit\"></i>");
+
             currentlyEditingRow = true;
         }
     }
+
 }
 
 function addToTable(event) {
+    //This makes sure if there was an error before, that the previous warning is removed.
+    $("#hours").removeAttr("title");
+
     var tableRow = {};
     tableRow.org = $("#org").val();
-    tableRow.hours = $("#hours").val();
-    tableRow.turnedIn = $("#turned-in").val();
+    tableRow.hours = processNumber("#hours");
+    if (tableRow.hours == "bad") {
+        $("#hours").prop("title", "You entered an invalid amount of hours.").tooltip().effect("highlight", 1000);;
+        return;
+    }
+
+    tableRow.turnedIn = processCheckMarkIn("#turned-in");
     tableRow.datePicker = $("#datepicker").val();
 
     if (currentlyEditingRow) {
+        //Set this to false for the next time the add button is clicked.
         currentlyEditingRow = false;
+
         $("#row" + numOfRow).replaceWith("<tr id=\"row" + numOfRow + "\"  class=\"row\"><td>" + tableRow.org + "</td><td class=\"hours\">" + tableRow.hours + "</td><td>" + tableRow.turnedIn + "</td><td>" + tableRow.datePicker + "</td><td><span class=\"editbox\"> </span></td></tr>");
+        showControls();
+
         tableRowArray[numOfRow] = tableRow;
+
+        $("#submit").html("<i class=\"fa fa-plus\"></i>");
+
+        //This stops the rest of the code in the addToTable function from running.
         return;
     }
 
@@ -77,16 +119,44 @@ function addToTable(event) {
 
     console.log(tableRowArray);
 
-        //Gives error:
-    //$("#add-row").insertBefore($("#add-row").before);
-
     tableRowArray.push(tableRow);
 
-    //Show and hide delete button
-    $(".editbox").parent().hover(showControls, hideControls);
-
+    //Show the delete and edit button when editbox is hovered
+    $(".editbox").parent().hover(showControls);
 
     return tableRow;
+}
+
+function processNumber(hours) {
+    var hoursVal = $(hours).val();
+    if (hoursVal % 1 !== 0 || hoursVal < 0) {
+        return "bad";
+    } else {
+        return hoursVal;
+    }
+
+
+}
+
+function processCheckMarkIn(checkMarkId) {
+    var checkMarkValue = $(checkMarkId + ":checked");
+
+    /*While experimenting, I discovered that checkMarkValue returns an object with an array that has a length of zero when unchecked and a length of 1 when checked.
+    */
+    if (checkMarkValue.length) {
+        return "yes";
+    } else {
+        return "no";
+    }
+    return checkMarkValue;
+}
+
+function processCheckMarkOut(checkMarkValue, checkMarkId) {
+    if (checkMarkValue == "yes") {
+        $(checkMarkId).prop("checked", true);
+    } else {
+        $(checkMarkId).prop("checked", false);
+    }
 }
 
 function sumUpHours() {
